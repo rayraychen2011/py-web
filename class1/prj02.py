fa3a2c606ba72e9a30ce87872c98be870ef2ec8e
rayraychen2011 <rayraychen2011@gmail.com>
2026-03-08 11:00:29 +0800

    新增 prj01.py 和 prj02.py 檔案，實作基本的 GUI 功能與按鈕事件

--- class1/prj02.py
+++ class1/prj02.py
@@ -3,18 +3,22 @@ from tkinter import *  # 匯入tkinter模組
 
 
 ##########################函數定義############################
-def hi_fun():
-    print("Hi, everyone!")
+clear_fun = lambda: display.config(text="")
+show_fun = lambda: display.config(text="Hello, world!")
 
 
 ##########################建立視窗############################
 windows = Tk()  # 建立視窗
 windows.title("My first GUI")  # 設定視窗標題
 btn1 = Button(
-    windows, text="click me", command=hi_fun
+    windows, text="show screen", command=show_fun
 )  # 建立按鈕,並指定按下按鈕後要執行的函數
 btn1.pack()  # 將按鈕放入視窗中
-display = Label(windows, text="Hello, world!", fg="red", bg="black")  # 建立標籤
+btn2 = Button(
+    windows, text="clear screen", command=clear_fun
+)  # 建立按鈕,並指定按下按鈕後要
+btn2.pack()  # 將按鈕放入視窗中
+display = Label(windows, text="")
 display.pack()  # 將標籤放入視窗中
 ########################運行應用程式##########################
 windows.mainloop()  # 開始執行主迴圈,讓視窗保持顯示狀態
