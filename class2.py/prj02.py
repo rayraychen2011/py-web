a5772f0a88f2045af2505e30abae83b426be5eb4
rayraychen2011 <rayraychen2011@gmail.com>
2026-03-15 11:05:52 +0800

    新增 prj01.py 和 prj02.py 檔案，實作 GUI 功能與顏色變更

--- class2.py/prj02.py
+++ class2.py/prj02.py
@@ -1,29 +1,29 @@
 ##########################匯入模組############################
 from tkinter import *  # 匯入tkinter模組
+import random
 
 
 ##########################函數定義############################
-def clear_fun():
-    bg_color = display.cget("bg")  # 使用cget獲取背景色
-    print(f"背景色: {bg_color}")  # 偵測並顯示背景色
-    display.config(text="")  # 清除文字
+def hi_fun():
+    fg_color = "#" + "".join(
+        [random.choice("0123456789ABCDEF") for j in range(6)]
+    )  # 隨機生成顏色
 
-
-show_fun = lambda: display.config(text="Hello, world!")
+    bg_color = "#" + "".join(
+        [random.choice("0123456789ABCDEF") for j in range(6)]
+    )  # 隨機生成顏色
+    display.config(text="Hello, world!", fg=fg_color, bg=bg_color)
 
 
 ##########################建立視窗############################
 windows = Tk()  # 建立視窗
 windows.title("My first GUI")  # 設定視窗標題
+windows.option_add("*Font", "Arial 20")  # 設定全局字體
 btn1 = Button(
-    windows, text="show screen", command=show_fun
+    windows, text="click me", command=hi_fun
 )  # 建立按鈕,並指定按下按鈕後要執行的函數
 btn1.pack()  # 將按鈕放入視窗中
-btn2 = Button(
-    windows, text="clear screen", command=clear_fun
-)  # 建立按鈕,並指定按下按鈕後要
-btn2.pack()  # 將按鈕放入視窗中
-display = Label(windows, text="")
+display = Label(windows, text="Hello, world!", fg="red", bg="black")  # 建立標籤
 display.pack()  # 將標籤放入視窗中
 ########################運行應用程式##########################
 windows.mainloop()  # 開始執行主迴圈,讓視窗保持顯示狀態
--- class2.py/test.py
+++ class2.py/test.py
@@ -3,27 +3,21 @@ from tkinter import *  # 匯入tkinter模組
 
 
 ##########################函數定義############################
-def clear_fun():
-    bg_color = display.cget("bg")  # 使用cget獲取背景色
-    print(f"背景色: {bg_color}")  # 偵測並顯示背景色
-    display.config(text="")  # 清除文字
-
-
-show_fun = lambda: display.config(text="Hello, world!")
+def hi_fun():
+    if display.cget("bg") == "red":
+        display.config(text="green", fg="black", bg="green")
+    elif display.cget("bg") == "green":
+        display.config(text="red", fg="black", bg="red")
 
 
 ##########################建立視窗############################
 windows = Tk()  # 建立視窗
 windows.title("My first GUI")  # 設定視窗標題
 btn1 = Button(
-    windows, text="show screen", command=show_fun
+    windows, text="click me", command=hi_fun
 )  # 建立按鈕,並指定按下按鈕後要執行的函數
 btn1.pack()  # 將按鈕放入視窗中
-btn2 = Button(
-    windows, text="clear screen", command=clear_fun
-)  # 建立按鈕,並指定按下按鈕後要
-btn2.pack()  # 將按鈕放入視窗中
-display = Label(windows, text="")
+display = Label(windows, text="red", fg="black", bg="red")  # 建立標籤
 display.pack()  # 將標籤放入視窗中
 ########################運行應用程式##########################
 windows.mainloop()  # 開始執行主迴圈,讓視窗保持顯示狀態
